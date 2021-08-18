ab467fa15d9fdae0581c1a5b8d07ad5d64a0f151
UserJA01 <A01633991@itesm.mx>
2021-08-18 03:13:23 +0000

    Solved test-lab real

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -7,5 +7,16 @@ import (
 
 func main() {
 	//fmt.Println("Welcome to the jungle")
-	fmt.Println("Welcome to the jungle", os.Args[1])
+	//fmt.Println("Welcome to the jungle", os.Args[1])
+	totalInput := len(os.Args)
+	if(totalInput < 2){
+		fmt.Println("Please write down at least one name")
+	}else{
+		fmt.Print("Welcome to the jungle ")
+		for i := 1; i < totalInput; i++ {
+			fmt.Print(os.Args[i])
+			fmt.Print(" ")
+		}
+	}
+	fmt.Println()
 }
