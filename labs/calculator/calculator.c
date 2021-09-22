591d5c263b85ebf5fd3c393245160fa9ce090f87
UserJA01 <A01633991@itesm.mx>
2021-09-22 00:31:33 +0000

    Calculator

--- labs/calculator/calculator.c
+++ labs/calculator/calculator.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-// adds/subtracts/multiplies all values that are in the *values array.
-// nValues is the number of values you're reading from the array
-// operator will indicate if it's an addition (1), subtraction (2) or
-// multiplication (3)
-long calc(int operator, int nValues, int *values) {
+int main(int argc, char **argv)
+{
+    int option = 0;
+    if(argc >= 2){
+        if(strcmp(argv[1], "add") == 0){
+            //printf("CORRECT!\n");
+            option = 0;
+        }else if(strcmp(argv[1], "sub") == 0){
+            option = 1;
+        }else if(strcmp(argv[1], "mult") == 0){
+            option = 2;
+        }
+        else{
+            //printf("Please write down your input in the next format: ");
+            printf("Please write down your operator(add,sub or multi) followed by the numbers you want\n");
+            printf("Example:\n");
+            printf("add 5 2 1 5\n");
+            exit(EXIT_FAILURE);
+        }
+        
+        int result = atoi(argv[2]);
+        //printf("Result: %d \n", result);
+        for(int x = 3; x < argc; x++){
+            if(isdigit(atoi(argv[x])) != 0){
+                /*int ax = atoi(argv[x]);
+                result = 123;*/
+                exit(EXIT_FAILURE);
+            }
+            if(option == 0){
+                result += atoi(argv[x]);
+            }else if(option == 1){
+                result -= atoi(argv[x]);
+            }else if(option == 2){
+                result *= atoi(argv[x]);
+            }
+            
+        }
+
+        printf("%d \n", result);
+    }
     return 0;
-}
-
-int main() {
-    return -1;
 }
